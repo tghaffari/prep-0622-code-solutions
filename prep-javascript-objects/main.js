6fff0c0bf9b55f57d5c061bb8106c7303f8e369a
Tarra Ghaffari <tarrag2525@gmail.com>
2022-06-17 15:33:59 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,16 @@
+var person = {
+  firstName: 'Tarra',
+  lastName: 'Ghaffari',
+  hobby: 'music'
+};
+console.log(person);
+
+console.log('The person\'s name is:', person.firstName + ' ' + person.lastName);
+
+person.job = 'Legal Coordinator at MGM Studios';
+console.log('This person\'s current job is:', person.job);
+
+person.previousJob = 'Research Assistant at Chapman School of Pharmacy';
+console.log('This person\'s previous job was:', person.previousJob);
+
+console.log(person);
